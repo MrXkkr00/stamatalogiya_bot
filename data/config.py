434250dc9caa2b63d9d8ef3c67c84257a29b7f5b
RasulbekQadomboyev <rasqadomboyev@gmail.com>
2024-09-03 12:41:00 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,11 +1,11 @@
 
 
-BOT_TOKEN = "6611923612:AAE0vDI_I-9BBbsDX554XarAVKvBgjCKNuk"  # Bot toekn
-ADMINS = ["984568970", "141829192", "60616510", "60616510"]
+BOT_TOKEN = ""  # Bot toekn
+ADMINS = ["", "", "", ""]
 Doctors = [0, "Якубов Дильшад", "Якубов Дилмурод", "Лор Врач"]
 IP = "localhost"
 
-DB_USER = "botlaru1_stg_user"
-DB_PASS = "12345rasulbek"
-DB_NAME = "botlaru1_stg_baza"
+DB_USER = ""
+DB_PASS = ""
+DB_NAME = ""
 DB_HOST = "localhost"
